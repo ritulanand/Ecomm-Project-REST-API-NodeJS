d73af8cb4e2b77c3000603382301a49a41fa742a
ritulanand <ritulanand007@gmail.com>
2025-01-24 12:31:20 +0530

    swagger removed to deploy on render

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //1 import exprerss
 import "./env.js";
 import express from "express";
-import swagger from "swagger-ui-express";
+// import swagger from "swagger-ui-express";
 import cors from "cors";
 
 import productRouter from "./src/features/product/product.routes.js";
@@ -11,7 +11,7 @@ import userRouter from "./src/features/user/user.routes.js";
 import jwtAuth from "./src/middlewares/jwt.middleware.js";
 import cartRouter from "./src/features/cartItems/cartItems.routes.js";
 
-import apiDocs from "./swagger.json" assert { type: "json" };
+// import apiDocs from "./swagger.json" assert { type: "json" };
 import loggerMiddleware from "./src/middlewares/logger.middleware.js";
 import { ApplicationError } from "./src/error-handler/applicationError.js";
 import { connectToMongoDB } from "./src/config/mongodb.js";
@@ -46,7 +46,7 @@ server.use(cors(corsOptions));
 server.use(bodyParser.json()); // application level middleware added for every req body
 // Bearer <token>
 
-server.use("/api-docs", swagger.serve, swagger.setup(apiDocs));
+// server.use("/api-docs", swagger.serve, swagger.setup(apiDocs));
 
 server.use(loggerMiddleware);
 
